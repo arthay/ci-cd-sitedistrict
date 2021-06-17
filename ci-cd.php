4f84163ad817c727ce86d2ce483819a449e766aa
arthay <arthur15251@gmail.com>
2021-06-17 10:33:10 +0400

    first commit

---
+++ ci-cd.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name:       CI-CD
+ * Description:       Example block written with ESNext standard and JSX support – build step required.
+ * Requires at least: 5.7
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       ci-cd
+ *
+ * @package           create-block
+ */
+
+function cc_load_footer_scripts() {
+	$index_js     = 'scripts/index.js';
+	wp_enqueue_script('script-name', plugins_url( $index_js, __FILE__ ), false, '1.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'cc_load_footer_scripts' );
